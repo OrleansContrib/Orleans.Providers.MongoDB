f4d6d9ac3ad8ac7eba2a0dafb8a33f54c38167b9
Nicholas <nicholas@bluewindowltd.com>
2019-06-10 19:37:34 +0200

    Replace order of default settings

--- Orleans.Providers.MongoDB/StorageProviders/Serializers/JsonGrainStateSerializer.cs
+++ Orleans.Providers.MongoDB/StorageProviders/Serializers/JsonGrainStateSerializer.cs
@@ -14,15 +14,15 @@ namespace Orleans.Providers.MongoDB.StorageProviders.Serializers
         public JsonGrainStateSerializer(ITypeResolver typeResolver, IGrainFactory grainFactory, MongoDBGrainStorageOptions options)
         {
             var jsonSettings = OrleansJsonSerializer.GetDefaultSerializerSettings(typeResolver, grainFactory);
-            options?.ConfigureJsonSerializerSettings?.Invoke(jsonSettings);
-            this.serializer = JsonSerializer.Create(jsonSettings);
-
+            
             //// https://github.com/OrleansContrib/Orleans.Providers.MongoDB/issues/44
             //// Always include the default value, so that the deserialization process can overwrite default 
             //// values that are not equal to the system defaults.
             this.serializer.NullValueHandling = NullValueHandling.Include;
-
             this.serializer.DefaultValueHandling = DefaultValueHandling.Populate;
+
+            options?.ConfigureJsonSerializerSettings?.Invoke(jsonSettings);
+            this.serializer = JsonSerializer.Create(jsonSettings);
         }
 
         public void Deserialize(IGrainState grainState, JObject entityData)
