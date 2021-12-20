6463eb7a66e8a0c34f5be56e77f21ec8fcf85d7a
Diego Frata <diego.frata@starlizard.com>
2021-12-20 15:01:29 +0000

    Adds the ability to use a different strategy for creating keys for stored states.

--- Orleans.Providers.MongoDB/Configuration/MongoDBGrainStorageOptions.cs
+++ Orleans.Providers.MongoDB/Configuration/MongoDBGrainStorageOptions.cs
@@ -2,6 +2,7 @@
 
 using Newtonsoft.Json;
 using System;
+using Orleans.Runtime;
 
 namespace Orleans.Providers.MongoDB.Configuration
 {
@@ -16,5 +17,25 @@ namespace Orleans.Providers.MongoDB.Configuration
         }
 
         public Action<JsonSerializerSettings> ConfigureJsonSerializerSettings { get; set; }
+
+        /// <summary>
+        /// The key generation strategy used by the storage provider. It defaults to calling ToKeyString
+        /// in the grain reference.
+        /// </summary>
+        public GrainStorageKeyGenerator KeyGenerator { get; set; } = x => x.ToKeyString();
+
+        internal override void Validate(string name = null)
+        {
+            base.Validate(name);
+
+            if (KeyGenerator == null)
+                throw new OrleansConfigurationException($"{nameof(KeyGenerator)} is required and cannot be null.");
+
+        }
     }
+
+    /// <summary>
+    /// Delegate representing a strategy for generating the key of the persisted state in MongoDB.
+    /// </summary>
+    public delegate string GrainStorageKeyGenerator(GrainReference grainReference);
 }
