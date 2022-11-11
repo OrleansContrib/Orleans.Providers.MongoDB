c723c8bcca07794a770c74f6d01c47d5093b3492
Markus Geiger <external.markus.geiger@bosch,com>
2022-11-11 17:52:44 +0100

    fixed indention

--- Test/Host/TestConfigurator.cs
+++ Test/Host/TestConfigurator.cs
@@ -10,65 +10,65 @@ using System.Net;
 
 namespace Orleans.Providers.MongoDB.Test.Host
 {
-  internal class TestConfigurator : ISiloConfigurator
-  {
-    readonly string connectionString = "mongodb://localhost/OrleansTestApp";
-    readonly bool createShardKey = false;
-    public void Configure(ISiloBuilder siloBuilder)
+    internal class TestConfigurator : ISiloConfigurator
     {
-      siloBuilder.UseMongoDBClient(connectionString)
-          .UseMongoDBClustering(options =>
-          {
-            options.DatabaseName = "OrleansTestApp";
-            options.CreateShardKeyForCosmos = createShardKey;
-          })
-          .AddStartupTask(async (s, ct) =>
-          {
-            var grainFactory = s.GetRequiredService<IGrainFactory>();
+        readonly string connectionString = "mongodb://localhost/OrleansTestApp";
+        readonly bool createShardKey = false;
+        public void Configure(ISiloBuilder siloBuilder)
+        {
+            siloBuilder.UseMongoDBClient(connectionString)
+                .UseMongoDBClustering(options =>
+                {
+                    options.DatabaseName = "OrleansTestApp";
+                    options.CreateShardKeyForCosmos = createShardKey;
+                })
+                .AddStartupTask(async (s, ct) =>
+                {
+                    var grainFactory = s.GetRequiredService<IGrainFactory>();
 
-            await grainFactory.GetGrain<IHelloWorldGrain>((int)DateTime.UtcNow.TimeOfDay.Ticks).SayHello("HI");
-          })
-          .UseMongoDBReminders(options =>
-          {
-            options.DatabaseName = "OrleansTestApp";
-            options.CreateShardKeyForCosmos = createShardKey;
-          })
-          .AddBroadcastChannel("OrleansTestStream", options =>
-          {
-            options.FireAndForgetDelivery = true;
-          })
-          .AddMongoDBGrainStorage("PubSubStore", options =>
-          {
-            options.DatabaseName = "OrleansTestAppPubSubStore";
-            options.CreateShardKeyForCosmos = createShardKey;
+                    await grainFactory.GetGrain<IHelloWorldGrain>((int)DateTime.UtcNow.TimeOfDay.Ticks).SayHello("HI");
+                })
+                .UseMongoDBReminders(options =>
+                {
+                    options.DatabaseName = "OrleansTestApp";
+                    options.CreateShardKeyForCosmos = createShardKey;
+                })
+                .AddBroadcastChannel("OrleansTestStream", options =>
+                {
+                    options.FireAndForgetDelivery = true;
+                })
+                .AddMongoDBGrainStorage("PubSubStore", options =>
+                {
+                    options.DatabaseName = "OrleansTestAppPubSubStore";
+                    options.CreateShardKeyForCosmos = createShardKey;
 
-            options.ConfigureJsonSerializerSettings = settings =>
-            {
-              settings.NullValueHandling = NullValueHandling.Include;
-              settings.ObjectCreationHandling = ObjectCreationHandling.Replace;
-              settings.DefaultValueHandling = DefaultValueHandling.Populate;
-            };
-          })
-          .AddMongoDBGrainStorage("MongoDBStore", options =>
-          {
-            options.DatabaseName = "OrleansTestApp";
-            options.CreateShardKeyForCosmos = createShardKey;
+                    options.ConfigureJsonSerializerSettings = settings =>
+              {
+                      settings.NullValueHandling = NullValueHandling.Include;
+                      settings.ObjectCreationHandling = ObjectCreationHandling.Replace;
+                      settings.DefaultValueHandling = DefaultValueHandling.Populate;
+                  };
+                })
+                .AddMongoDBGrainStorage("MongoDBStore", options =>
+                {
+                    options.DatabaseName = "OrleansTestApp";
+                    options.CreateShardKeyForCosmos = createShardKey;
 
-            options.ConfigureJsonSerializerSettings = settings =>
-            {
-              settings.NullValueHandling = NullValueHandling.Include;
-              settings.ObjectCreationHandling = ObjectCreationHandling.Replace;
-              settings.DefaultValueHandling = DefaultValueHandling.Populate;
-            };
+                    options.ConfigureJsonSerializerSettings = settings =>
+              {
+                      settings.NullValueHandling = NullValueHandling.Include;
+                      settings.ObjectCreationHandling = ObjectCreationHandling.Replace;
+                      settings.DefaultValueHandling = DefaultValueHandling.Populate;
+                  };
 
-          })
-          .Configure<ClusterOptions>(options =>
-          {
-            options.ClusterId = "helloworldcluster";
-            options.ServiceId = "helloworldcluster";
-          })
-          .ConfigureEndpoints(IPAddress.Loopback, 11111, 30000)
-          .ConfigureLogging(logging => logging.AddConsole());
+                })
+                .Configure<ClusterOptions>(options =>
+                {
+                    options.ClusterId = "helloworldcluster";
+                    options.ServiceId = "helloworldcluster";
+                })
+                .ConfigureEndpoints(IPAddress.Loopback, 11111, 30000)
+                .ConfigureLogging(logging => logging.AddConsole());
+        }
     }
-  }
 }
