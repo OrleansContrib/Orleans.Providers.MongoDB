408d5f6760ced730888893dcc8f2e69387435a9e
Tomas Hrebicek <tomas.hrebicek@tietoevry.com>
2023-03-21 12:46:11 +0100

    #124 Unique constraint violations are not propagated

---
+++ Test/GrainInterfaces/IConstrainedGrain.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Orleans.Providers.MongoDB.Test.GrainInterfaces
+{
+    public interface IConstrainedGrain : IGrainWithIntegerKey
+    {
+        Task SetName(string name);
+    }
+}
