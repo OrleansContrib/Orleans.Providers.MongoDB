104845c4656890a7e52a775c75402fd1f719525d
wassim-k <wassim.khalil9@gmail.com>
2024-07-25 11:32:33 +1000

    Create a new JSON serializer instance to avoid thread-safety issues

--- Orleans.Providers.MongoDB/StorageProviders/Serializers/JsonGrainStateSerializer.cs
+++ Orleans.Providers.MongoDB/StorageProviders/Serializers/JsonGrainStateSerializer.cs
@@ -10,24 +10,26 @@ namespace Orleans.Providers.MongoDB.StorageProviders.Serializers
 {
     public class JsonGrainStateSerializer : IGrainStateSerializer
     {
-        private readonly JsonSerializer serializer;
+        private readonly JsonSerializerSettings _jsonSettings;
 
         public JsonGrainStateSerializer(IOptions<JsonGrainStateSerializerOptions> options, IServiceProvider serviceProvider)
         {
-            var jsonSettings = OrleansJsonSerializerSettings.GetDefaultSerializerSettings(serviceProvider);
-            options.Value.ConfigureJsonSerializerSettings(jsonSettings);
-            serializer = JsonSerializer.CreateDefault(jsonSettings);
+            _jsonSettings = OrleansJsonSerializerSettings.GetDefaultSerializerSettings(serviceProvider);
+            options.Value.ConfigureJsonSerializerSettings(_jsonSettings);
         }
 
         public T Deserialize<T>(BsonValue value)
         {
             using var jsonReader = new JTokenReader(value.ToJToken());
-            return serializer.Deserialize<T>(jsonReader);
+            // Creating a new serializer instance to avoid thread-safety issue: https://github.com/JamesNK/Newtonsoft.Json/issues/1452
+            var jsonSerializer = JsonSerializer.CreateDefault(_jsonSettings);
+            return jsonSerializer.Deserialize<T>(jsonReader);
         }
 
         public BsonValue Serialize<T>(T state)
         {
-            return JObject.FromObject(state, serializer).ToBson();
+            var jsonSerializer = JsonSerializer.CreateDefault(_jsonSettings);
+            return JObject.FromObject(state, jsonSerializer).ToBson();
         }
     }
 }
